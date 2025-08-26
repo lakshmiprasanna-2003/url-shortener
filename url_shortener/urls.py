8895639277c5b8beba6e133d77544dea2f2f186e
Prasanna <lakshiprasannadesireddy2@gmail.com>
2025-08-26 16:12:42 +0530

    errors resolved in redirecting urls

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -3,9 +3,10 @@ from django.urls import path,include
 from . import views
  
 urlpatterns = [
-    path('hello',views.hello_world),
-    path('task',views.task),
-    path('',views.home_page),
-    path('analytics',views.analytics),
-    path('<slug:customname>',views.redirect_url)
+    path('hello', views.hello_world),
+    path('task', views.task),
+    path('', views.home_page),
+    path('analytics', views.analytics),
+    path('redirect/<str:customname>/', views.redirect_url, name='redirect_url'),
 ]
+
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,65 +1,67 @@
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from url_shortener.models import longtoshort
-
 
 
 # Create your views here.
 def hello_world(request):
     return HttpResponse("Hello how are you")
 
+
 def task(request):
-    context={"year":"2023","attendees":["lakshmi","prasanna","pragathi","saranya"]}
-    return render(request,"task.html",context)
+    context = {
+        "year": "2023",
+        "attendees": ["lakshmi", "prasanna", "pragathi", "saranya"]
+    }
+    return render(request, "task.html", context)
+
 
 def home_page(request):
-    context={
-        "submitted":False,
-        "error":False
-             
+    context = {
+        "submitted": False,
+        "error": False
     }
-    print(request.META)
 
-    if request.method=="POST":
-        #print(request.POST)
-        data=request.POST
-        longurl=data['longurl']
-        customname=data['custom_name']
+    if request.method == "POST":
+        data = request.POST
+        longurl = data['longurl']
+        customname = data['custom_name']
 
         try:
-
-            context["long_url"]=longurl
-            context["custom_name"]=request.build_absolute_uri() + customname
-            customname=request.build_absolute_uri() + customname
-            obj =longtoshort(long_url=longurl,custom_name=customname)
+            # save only the slug (customname) in DB
+            obj = longtoshort(long_url=longurl, custom_name=customname)
             obj.save()
-            context["submitted"]=True
-            context["date"]=obj.create_date
-            context["clicks"]=obj.vist_count
-            
-        except:
-            context["error"]=True    
-    
-        #print(longurl,customname)       
 
-    else:
-        print("User didn't submit yet")
-    return render(request,"index.html",context)
+            # display shortened full URL to user
+            short_url = request.build_absolute_uri(customname)
 
-def redirect_url(request,customname):
-    row=longtoshort.objects.filter(custom_name=customname)
-    if len(row)==0:
-        return HttpResponse("this endpoint doesn't exist error!!")
-    obj=row[0]
-    long_url=obj.long_url
+            context["long_url"] = longurl
+            context["custom_name"] = short_url
+            context["submitted"] = True
+            context["date"] = obj.create_date
+            context["clicks"] = obj.vist_count
+
+        except Exception as e:
+            print("Error:", e)
+            context["error"] = True
+
+    return render(request, "index.html", context)
+
+
+def redirect_url(request, customname):
+    row = longtoshort.objects.filter(custom_name=customname)
+    if not row.exists():
+        return HttpResponse("This endpoint doesn't exist error!!")
+    obj = row.first()
+    long_url = obj.long_url
     obj.vist_count += 1
     obj.save()
     return redirect(long_url)
 
+
 def analytics(request):
-    rows=longtoshort.objects.all()
-    context={
-        "rows":rows
+    rows = longtoshort.objects.all()
+    context = {
+        "rows": rows
     }
-    return render(request,"analytics.html",context)
+    return render(request, "analytics.html", context)
